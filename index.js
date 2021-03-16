a779eb6a76fa2742757ca0db5629516bfa432f37
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-16 04:35:06 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(teams) {
+  let winningTeam = teams.find(teams => teams.result === "W");
+  if (winningTeam) {
+    return winningTeam.year;
+  } else {
+    return undefined;
+  }
+}
